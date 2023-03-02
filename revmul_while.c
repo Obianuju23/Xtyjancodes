8b8f8ee9bcae71c046f7292d59a418e58e28f606
Obianuju23 <obianujunmoh@gmail.com>
2023-03-02 04:45:43 -0800

    program that prints multiplication table in a reverse order using while loop

---
+++ revmul_while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    int number;
+    int count =12;
+   
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    
+    while (count < 13 && count >= 0)
+    {
+        int product = number*count;
+        printf("%d X %d = %d\n", number, count, product);
+        count = count - 1;
+    }
+    return 0;
+}
+
+
+/*Create a program that prints the multiplication table
+for a given number.print the number from 12 to 1.*/
