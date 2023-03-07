024c6972e256cb812e3ad072afbbc582b94754d5
Obianuju23 <obianujunmoh@gmail.com>
2023-03-07 04:19:36 -0800

    Can you create a program to convert distance in kilometers to miles

--- ConKm_Mi.py
+++ ConKm_Mi.py
@@ -1,4 +1,8 @@
 #program to convert kilometers to miles
+#Can you create a program to convert distance in kilometers to miles?
+
+#Formula: 1 kilometer -> 0.621371 miles
+
 
 Distance_km = 564.5
 Miles = 0.621371
