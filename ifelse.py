f3402a24ddb8e2384e558743966b9e60c489fdad
Obianuju23 <obianujunmoh@gmail.com>
2023-03-08 03:59:13 -0800

    program that checks if students pass their exams using ifelse statement

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+score = int(input("Enter your score: "))
+
+if score >= 50:
+    print(f"You have passed your exams", {score})
+    print(f"Congratualtions!")
+else:
+    print(f"You have failed your exams", {score} )
+    print(f"Try harder next time")
+    
+    # program task
+    # Check if student passed the exam Suppose you are a university student and to pass the examination you need to score 50 or more. Lets look at a program to check if you passed the exam.
