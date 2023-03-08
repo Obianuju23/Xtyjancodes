166e1c643cae356b01a2b096a24b548118252e2b
Obianuju23 <obianujunmoh@gmail.com>
2023-03-08 15:01:00 -0800

    program that prints multiplication table using while loop

---
+++ mulinverted_whi.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number: "))
+
+count = 10
+
+while count > 0:
+    product = number * count
+    print(number,"X", count, "=", product)
+    count = count - 1
+    
+    
+    # Can you modify our multiplication table program so that we get a multiplication table from 10 to 1 instead of 1 to 10.
