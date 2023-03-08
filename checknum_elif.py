10caf42e1643afbcaf04ea3b55f66ff6f5c62170
Obianuju23 <obianujunmoh@gmail.com>
2023-03-08 14:00:48 -0800

    program to check whether a number is positive or negative or 0

---
+++ checknum_elif.py
@@ -0,0 +1,19 @@
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print(f"The number is positive")
+elif number == 0:
+    print(f"The number is 0")
+else:
+    print(f"The number is negative" )
+
+
+
+#Programming Task
+#Can you create a program to check whether a number is positive or negative or 0?
+
+#To create this program, create a variable named number and assign a float value to it based on the user input. Then using a if statement, check if the number variable is positive or negative or 0.
+
+#If number is positive, print "The number is positive"
+#If number is 0, print "The number is 0"
+#If number is negative, print "The number is negative"
