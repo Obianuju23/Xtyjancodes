5541ded88d7ae97d13ea5ba8b670144cd4b055e7
Obianuju23 <obianujunmoh@gmail.com>
2023-03-12 09:23:56 -0700

    program that prints a given row of pyramid

---
+++ pyramid.c
@@ -0,0 +1,44 @@
+/**
+ * Write a program that prints a given row of pyramid
+*/
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    int i, j, row, column;
+
+    //Prompt User for how many rows they need
+    printf("How many rows of Pyramid do you need?: ");
+    scanf("%d", &row);
+
+    while(row < 2)
+    {
+        printf("\a\nPlease Enter a number greater than 2");
+        printf("\nHow many rows of Pyramid do you need?: ");
+        scanf("%d", &row);
+    }
+
+    //Set conditional statements to print pyramid
+    for(i = 1; i <= row; i++)
+    {
+        for(j = 1; j <= 2 * row - 1; j++)
+        {
+            if(j >= row - (i - 1) && j <= row + (i - 1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\a\nIf you found this program helpful, please leave a like.\n\nAlso, if you have any questions, contributions or corrections, please comment it.\nThank you.\n");
+    
+
+    return 0;
+}
