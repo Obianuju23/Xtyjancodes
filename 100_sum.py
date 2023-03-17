8394c2f6207546555927e72cee5bd8389db970cf
Obianuju23 <obianujunmoh@gmail.com>
2023-03-17 11:57:12 -0700

    program to find the sum of numbers from 1 to 100.

---
+++ 100_sum.py
@@ -0,0 +1,9 @@
+# Can you create a program to find the sum of numbers from 1 to 100.
+
+# The result should be equal to 1 + 2 + 3 + ... + 100.
+# total = 0
+
+sum = 0
+for i in range(1,101):
+   sum = sum + i
+print(sum)
